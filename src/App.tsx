1ab84678fef4e123e02159f416f641f78d8b7306
Ana-Maria Grădinaru <anamariagradinaru92@yahoo.com>
2024-10-01 19:03:09 +0300

    Made the button to swich the light/dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { Button, Container, Flex, Heading } from '@chakra-ui/react';
 import './App.css';
 import SearchBar from './components/search-bar';
 import Card from './components/card';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
     return (
         <Container
             display={'flex'}
@@ -14,11 +24,12 @@ function App() {
             <Flex gap={'600px'} justifyContent={'center'} alignItems={'center'}>
                 <Heading color={'var(--heading-color)'}>devfinder</Heading>
                 <Button
+                    onClick={toggleTheme}
                     cursor={'pointer'}
                     backgroundColor={'var(--card-color)'}
                     border={'none'}
                 >
-                    Light{' '}
+                    {theme === 'light' ? 'Light' : 'Dark'} Theme
                 </Button>
             </Flex>
 
